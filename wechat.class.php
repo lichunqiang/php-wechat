e968bd38143ff2ac6cbba98a79590499244fb269
lichunqiang <light-li@hotmail.com>
2014-09-13 13:14:58 +0800

    add convert short url func

--- wechat.class.php
+++ wechat.class.php
@@ -71,6 +71,7 @@ class Wechat
 	const QR_SCENE = 0;
 	const QR_LIMIT_SCENE = 1;
 	const QRCODE_IMG_URL='https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=';
+	const SHORT_URL='/shorturl?';
 	const USER_GET_URL='/user/get?';
 	const USER_INFO_URL='/user/info?';
 	const USER_UPDATEREMARK_URL='/user/info/updateremark?';
@@ -940,6 +941,31 @@ class Wechat
 	 */
 	public function getQRUrl($ticket) {
 		return self::QRCODE_IMG_URL.$ticket;
+	}
+
+	/**
+	* 长链接转为短链接
+	* @param string $long_url 传入要转换的长url
+	* @return boolean|string url 成功则返回转换后的短url
+	*/
+	public function getShortUrl($long_url)
+	{
+		if (!$this->access_token && !$this->checkAuth()) return false;
+		$data = array(
+			'action'=>'long2short',
+			'long_url'=>$long_url
+		);
+		$result = $this->http_post(self::API_URL_PREFIX.self::SHORT_URL.'access_token='.$this->access_token,self::json_encode($data));
+		if ($result) {
+			$json = json_decode($result,true);
+			if (!$json || !empty($json['errcode'])) {
+				$this->errCode = $json['errcode'];
+				$this->errMsg = $json['errmsg'];
+				return false;
+			}
+			return $json['short_url'];
+		}
+		return false;
 	}
 
 	/**
